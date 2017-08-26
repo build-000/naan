b9d0a63af1c48a0d878b667027d0ac3d344d95ad
Moomin <bumbermin@gmail.com>
2017-08-27 07:42:21 +0900

    change custom icons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,123 @@
-/*
-	Ethereal by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+
+
+
+var prevTrack = 0;
+var currentTrack = 0;
+
+function playAudio(myaudio) {
+   /*
+   // play button change
+   document.getElementById("btnPlay").innerHTML = "Pause";
+   */
+  myaudio.play();
+   nextPlag = false;
+};
+function pauseAudio(myaudio) {
+   /*
+   // pause button change
+   document.getElementById("btnPlay").innerHTML = "Play";
+
+   */
+  myaudio.pause();
+};
+function togglePlay(myaudio) {
+  if (myaudio.paused == true){
+      console.log(myaudio.paused)
+    playAudio(myaudio);
+  }else{
+    pauseAudio(myaudio);
+  }
+};
+function selectTrack(track_num){
+  if (myaudio.childNodes[track_num]){
+    myaudio.src = myaudio.childNodes[track_num].src;
+  }
+};
+function getTrackNum(){
+  var track_num = Math.floor((Math.random() * myaudio.children.length));
+  if (track_num == currentTrack){
+    getTrackNum();
+  }else{
+    currentTrack = track_num;
+    return track_num;
+  }
+}
+function nextPlay(){
+  prevTrack = currentTrack;
+  selectTrack(getTrackNum());
+  document.getElementById('btnPlay').click();
+};
+function prevPlay(){
+  selectTrack(prevTrack);
+  document.getElementById('btnPlay').click();
+};
+
+var firstExcution = false;
+var getTracksDone = function(list){
+   console.log(list)
+  var myaudio = document.getElementById("myaudio");
+  var client_id = '761LMfrpB07DQlPhf7rbKo5fLsBuMaKH';
+
+   for (var i=0;i<list.length;i++){
+      var stream_url = list[i].stream_url + '?client_id=' + client_id;
+      var source = document.createElement('source');
+      source.setAttribute('src',stream_url);
+      myaudio.appendChild(source);
+
+   }
+
+   if (firstExcution == false){
+      console.log('first')
+      firstExcution = true;
+      $('#btnPlay').click();
+   }
+
+
+};
+
+var getTracks = function(){
+  var mood = 'happy';
+   navigator.geolocation.getCurrentPosition(function(position){
+      var url = 'https://91igu4dgel.execute-api.ap-northeast-2.amazonaws.com/prod/tracks?mood=' + mood + '&lat=' + position.coords.latitude + '&lng=' + position.coords.longitude;
+      $.ajax({
+         url: url
+      }).success(
+         getTracksDone.bind(this)
+      ).fail(function (e) {
+         // handle erorr
+         console.error("unexpected error: ", e);
+      });
+ });
+};
 
 (function($) {
+
+getTracks();
+
+   $('#btnPrev').bind('click',function(){
+      prevPlay();
+   });
+
+   $('#btnPlay').bind('click',function(){
+      var myaudio = document.getElementById("myaudio");
+    togglePlay(myaudio);
+   });
+
+	var nextPlag = false;
+   $('#btnNext').bind('click',function(){
+      if (nextPlag == false){
+         nextPlag = true;
+         nextPlay();
+      }
+   });
+
+   $('#myaudio').bind('playing',function(){
+      console.log('playing');
+   });
+
+   $('#myaudio').bind('ended',function(){
+      nextPlay();
+   });
 
 	// Settings.
 		var settings = {
