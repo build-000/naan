c8f2b23f454331532894c0f048ba917b52256308
ruucm <ruucm.a@gmail.com>
2017-09-12 07:22:27 +0900

    swlee

--- client-ng4/config/webpack.dist.config.js
+++
@@ -1,9 +0,0 @@
-//webpack.dist.config.js
-
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-
-config.plugins = config.plugins.concat([
-  new CopyWebpackPlugin([
-    { from: 'src/assets', to: 'src/assets' }
-  ]),
-]);
